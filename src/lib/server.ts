ec1c7f23a2b3c9c4b606fd628e148604915bb8ef
Chijioke <osadebec98@gmail.com>
2024-12-12 19:23:07 +0100

    Auto-commit: Thu Dec 12 07:23:07 PM WAT 2024

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,16 +1,92 @@
 import axios from "axios";
 
-const ADDRESS = "https://activity-tracker-backend-wtih.onrender.com";
-export async function check_user(userId: string) {
-  const response = await axios.post(`${ADDRESS}/check_user`, {
-    userId,
-  });
-  return response.data.isPresent;
+const ADDRESS = "http://localhost:3000";
+
+type TOG_Response = {
+  success: boolean;
+  og_response: string | null;
+  error: string | null;
+};
+export async function ogResponseServer(
+  userId: string,
+  prompt: string,
+): Promise<TOG_Response> {
+  try {
+    const response = await axios.post(`${ADDRESS}/og_response`, {
+      userId,
+      prompt,
+    });
+    if (response.status == 505 || response.status == 200) {
+      const responseData = response.data as TOG_Response;
+      return responseData;
+    } else {
+      throw new Error("Server issue");
+    }
+  } catch (error) {
+    const catchError = error as Error;
+    const response: TOG_Response = {
+      success: false,
+      og_response: null,
+      error: catchError.message,
+    };
+    return response;
+  }
 }
-export async function appendToDb(userId: string, encryptedContent: string) {
-  const response = await axios.post(`${ADDRESS}/append_to_db`, {
-    userId,
-    encryptedContent,
-  });
-  return response.data;
+
+type TCreateHTMLResponse = {
+  success: boolean;
+  html: string | null;
+  error: string | null;
+};
+export async function createHTMLServer(
+  userId: string,
+  prompt: string,
+): Promise<TCreateHTMLResponse> {
+  try {
+    const response = await axios.post(`${ADDRESS}/create_html`, {
+      userId,
+      prompt,
+    });
+    if (response.status == 505 || response.status == 200) {
+      const responseData = response.data as TCreateHTMLResponse;
+      return responseData;
+    } else {
+      throw new Error("Server issue");
+    }
+  } catch (error) {
+    const catchError = error as Error;
+    const response: TCreateHTMLResponse = {
+      success: false,
+      html: null,
+      error: catchError.message,
+    };
+    return response;
+  }
+}
+
+type TGetUserTrials = {
+  trials: number | null;
+  error?: string | null;
+};
+export async function getUserTrialsServer(
+  userId: string,
+): Promise<TGetUserTrials> {
+  try {
+    const response = await axios.post(`${ADDRESS}/get_user_trials`, {
+      userId,
+    });
+    if (response.status == 505 || response.status == 200) {
+      const responseData = response.data as TGetUserTrials;
+      return responseData;
+    } else {
+      throw new Error("Server issue");
+    }
+  } catch (error) {
+    const catchError = error as Error;
+    const errorResponse: TGetUserTrials = {
+      trials: null,
+      error: catchError.message,
+    };
+    return errorResponse;
+  }
 }
